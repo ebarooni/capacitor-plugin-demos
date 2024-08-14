5e3a5f0e036df30f111bc464e1e582b0d5dda0e2
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-14 19:56:07 +0200

    uses theme service in app component

--- projects/capacitor-calendar/src/app/app.component.spec.ts
+++ projects/capacitor-calendar/src/app/app.component.spec.ts
@@ -3,17 +3,33 @@ import { AppComponent } from './app.component';
 import { test, describe, expect, beforeEach } from 'vitest';
 import { By } from '@angular/platform-browser';
 import { appConfig } from './app.config';
+import { ThemeService } from './services/theme/theme.service';
+import { BehaviorSubject } from 'rxjs';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let themeService: ThemeService;
+
+  let isDarkModeSubject: BehaviorSubject<boolean>;
 
   beforeEach(async () => {
+    isDarkModeSubject = new BehaviorSubject<boolean>(false);
+    themeService = {
+      isDarkMode$: isDarkModeSubject.asObservable(),
+    };
+
     await TestBed.configureTestingModule(
       Object.assign({}, appConfig, {
         imports: [AppComponent],
       }),
-    ).compileComponents();
+    )
+      .overrideComponent(AppComponent, {
+        set: {
+          providers: [{ provide: ThemeService, useValue: themeService }],
+        },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
@@ -28,6 +44,7 @@ describe('AppComponent', () => {
   });
 
   test(`should have the 'ion-router-outlet' element`, () => {
+    fixture.detectChanges();
     expect(
       fixture.debugElement.query(By.css('ion-router-outlet')),
     ).toBeTruthy();
--- projects/capacitor-calendar/src/app/app.component.ts
+++ projects/capacitor-calendar/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { ThemeService } from './services/theme/theme.service';
+import { LetDirective } from '@ngrx/component';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, LetDirective],
   templateUrl: './app.component.html',
+  providers: [ThemeService],
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly themeService = inject(ThemeService);
+  readonly isDarkMode$ = this.themeService.isDarkMode$.pipe(
+    tap((isDarkMode) => document.body.classList.toggle('dark', isDarkMode)),
+  );
+}
