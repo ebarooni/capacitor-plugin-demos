aa5eb67ade863d8d4517db183c6c3e0e4226518d
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-14 19:09:55 +0200

    replaces placeholder template

--- projects/capacitor-calendar/src/app/app.component.ts
+++ projects/capacitor-calendar/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'capacitor-calendar';
-}
+export class AppComponent {}
