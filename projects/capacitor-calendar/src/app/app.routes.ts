879a514cabd3204934a67ccca8d4811314d340ee
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-15 19:37:01 +0200

    Defines route to api

--- projects/capacitor-calendar/src/app/app.routes.ts
+++ projects/capacitor-calendar/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: TabsComponent,
+    children: [
+      {
+        path: 'api',
+      },
+      {
+        path: '',
+        redirectTo: 'api',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/api',
+  },
+];
