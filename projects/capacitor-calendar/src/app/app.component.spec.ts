6dbc76b791e636df157a6d6bf879fc0bef6c1795
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-15 19:35:41 +0200

    Adds unit tests

--- projects/capacitor-calendar/src/app/app.component.spec.ts
+++ projects/capacitor-calendar/src/app/app.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { test, describe, expect, beforeEach } from 'vitest';
+import {
+  test,
+  describe,
+  expect,
+  beforeEach,
+  vi,
+  afterEach,
+  MockInstance,
+} from 'vitest';
 import { By } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { ThemeService } from './services/theme/theme.service';
 import { BehaviorSubject } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
   let themeService: ThemeService;
+  let scheduler: TestScheduler;
 
   let isDarkModeSubject: BehaviorSubject<boolean>;
+  let toggleSpy: MockInstance;
 
   beforeEach(async () => {
     isDarkModeSubject = new BehaviorSubject<boolean>(false);
@@ -33,6 +44,13 @@ describe('AppComponent', () => {
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).deep.equal(expected);
+    });
+  });
+
+  afterEach(() => {
+    toggleSpy?.mockRestore();
   });
 
   test('should create the app', () => {
@@ -49,4 +67,25 @@ describe('AppComponent', () => {
       fixture.debugElement.query(By.css('ion-router-outlet')),
     ).toBeTruthy();
   });
+
+  test.each([true, false])(
+    'should toggle dark class on document body',
+    (isDarkMode) => {
+      toggleSpy = vi.spyOn(document.body.classList, 'toggle');
+      const initialIsDarkMode = isDarkModeSubject.getValue();
+      scheduler.run(({ cold, expectObservable, flush }) => {
+        cold('-a').subscribe(() => isDarkModeSubject.next(isDarkMode));
+        expectObservable(component.isDarkMode$).toBe('ab', {
+          a: initialIsDarkMode,
+          b: isDarkMode,
+        });
+        flush();
+        expect(document.body.classList.toggle).toHaveBeenNthCalledWith(
+          2,
+          'dark',
+          isDarkMode,
+        );
+      });
+    },
+  );
 });
--- projects/capacitor-calendar/src/app/services/theme/theme.service.spec.ts
+++ projects/capacitor-calendar/src/app/services/theme/theme.service.spec.ts
@@ -1,20 +1,73 @@
 import { TestBed } from '@angular/core/testing';
-import { describe, beforeEach, it, expect } from 'vitest';
-import '../../../__mocks__/match-media';
-
+import {
+  describe,
+  beforeEach,
+  test,
+  expect,
+  vi,
+  afterAll,
+  MockInstance,
+} from 'vitest';
 import { ThemeService } from './theme.service';
+import { fromEvent, Subject } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+
+const matchMedia = vi.fn().mockImplementation((query) => ({
+  matches: false,
+  media: query,
+  onchange: null,
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+}));
+
+vi.stubGlobal('matchMedia', matchMedia);
+
+vi.mock('rxjs', async (importOriginal) => ({
+  ...(await importOriginal<typeof import('rxjs')>()),
+  fromEvent: vi.fn(),
+}));
 
 describe('ThemeService', () => {
   let service: ThemeService;
+  let fromEventSubject: Subject<MediaQueryListEvent>;
+  let scheduler: TestScheduler;
 
   beforeEach(() => {
+    fromEventSubject = new Subject<MediaQueryListEvent>();
+    (fromEvent as unknown as MockInstance).mockReturnValue(
+      fromEventSubject.asObservable(),
+    );
+
     TestBed.configureTestingModule({
       providers: [ThemeService],
     });
     service = TestBed.inject(ThemeService);
+
+    scheduler = new TestScheduler((actual, expected) => {
+      expect(actual).deep.equal(expected);
+    });
   });
 
-  it('should be created', () => {
+  afterAll(() => {
+    vi.unstubAllGlobals();
+  });
+
+  test('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  test.each([true, false])(
+    'should start with false initial dark mode followed by %s',
+    (isDarkMode) => {
+      const event = { matches: isDarkMode } as MediaQueryListEvent;
+      scheduler.run(({ cold, expectObservable }) => {
+        cold('-a').subscribe(() => fromEventSubject.next(event));
+        expectObservable(service.isDarkMode$).toBe('ab', {
+          a: false, // from the global stub above
+          b: event.matches,
+        });
+      });
+    },
+  );
 });
