13262ec68a259ebf08cef0a07265f1285ebddd96
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-16 21:55:25 +0200

    Implements api component hero

--- projects/capacitor-calendar/src/app/tabs/api/api.component.ts
+++ projects/capacitor-calendar/src/app/tabs/api/api.component.ts
@@ -1,18 +1,45 @@
 import { Component } from '@angular/core';
 import {
+  IonAvatar,
   IonChip,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonNote,
+  IonRow,
   IonSearchbar,
+  IonText,
   IonTitle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-api',
   templateUrl: './api.component.html',
   styleUrls: ['./api.component.scss'],
-  imports: [IonHeader, IonContent, IonToolbar, IonTitle, IonSearchbar, IonChip],
+  imports: [
+    IonHeader,
+    IonContent,
+    IonToolbar,
+    IonTitle,
+    IonSearchbar,
+    IonChip,
+    IonGrid,
+    IonList,
+    IonRow,
+    IonCol,
+    IonItem,
+    IonAvatar,
+    IonLabel,
+    IonText,
+    IonNote,
+    NgOptimizedImage,
+  ],
   standalone: true,
 })
 export class ApiComponent {
