ac5eab5342c48c313fe34f779ff6e1ddd1e927b3
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-17 23:17:48 +0200

    Adds Apple and Android icons

--- projects/capacitor-calendar/src/app/services/icons/icons.service.ts
+++ projects/capacitor-calendar/src/app/services/icons/icons.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
 import { addIcons } from 'ionicons';
-import { codeSlash, clipboard, settings } from 'ionicons/icons';
+import {
+  codeSlash,
+  clipboard,
+  settings,
+  logoApple,
+  logoAndroid,
+} from 'ionicons/icons';
 
 @Injectable({ providedIn: 'root' })
 export class IconsService {
-  private static readonly ICONS = { codeSlash, clipboard, settings };
+  private static readonly ICONS = {
+    codeSlash,
+    clipboard,
+    settings,
+    logoApple,
+    logoAndroid,
+  };
 
   registerIcons(): void {
     addIcons(IconsService.ICONS);
