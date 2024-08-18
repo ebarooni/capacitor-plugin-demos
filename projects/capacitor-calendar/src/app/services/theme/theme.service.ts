b6bf5be98ad78a6f94778906bf22679303efe044
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-18 18:39:39 +0200

    Implements manual dark mode

--- projects/capacitor-calendar/src/app/services/theme/theme.service.ts
+++ projects/capacitor-calendar/src/app/services/theme/theme.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { fromEvent, map, shareReplay, startWith } from 'rxjs';
+import { fromEvent, map, merge, shareReplay, startWith, Subject } from 'rxjs';
 
 @Injectable()
 export class ThemeService {
-  readonly isDarkMode$ = fromEvent<MediaQueryListEvent>(
-    window.matchMedia('(prefers-color-scheme: dark)'),
-    'change',
-  ).pipe(
-    startWith(window.matchMedia('(prefers-color-scheme: dark)')),
-    map((event) => event.matches),
-    shareReplay(1),
-  );
+  private readonly manualIsDarkModeSubject = new Subject<boolean>();
+  readonly isDarkMode$ = merge(
+    fromEvent<MediaQueryListEvent>(
+      window.matchMedia('(prefers-color-scheme: dark)'),
+      'change',
+    ).pipe(
+      startWith(window.matchMedia('(prefers-color-scheme: dark)')),
+      map((event) => event.matches),
+    ),
+    this.manualIsDarkModeSubject,
+  ).pipe(shareReplay(1));
+
+  enableDarkMode(enable: boolean): void {
+    this.manualIsDarkModeSubject.next(enable);
+  }
 }
--- projects/capacitor-calendar/src/app/tabs/settings/settings.component.ts
+++ projects/capacitor-calendar/src/app/tabs/settings/settings.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonItem,
+  IonList,
+  IonRow,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from '@ionic/angular/standalone';
+import { AvatarBannerComponent } from '../../shared-components/avatar-banner/avatar-banner.component';
+import { ThemeService } from '../../services/theme/theme.service';
+import { LetDirective } from '@ngrx/component';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   standalone: true,
-  imports: [IonHeader, IonTitle, IonToolbar, IonContent],
+  imports: [
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    AvatarBannerComponent,
+    IonList,
+    IonItem,
+    IonToggle,
+    LetDirective,
+  ],
 })
-export class SettingsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class SettingsComponent {
+  constructor(readonly themeService: ThemeService) {}
 }
