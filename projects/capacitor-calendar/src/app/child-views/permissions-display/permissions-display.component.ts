b5156a66889fdfee2c6c85da93e1804bf06b7bb7
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-08-19 21:38:02 +0200

    Deletes base-ion-content.component.ts

--- projects/capacitor-calendar/src/app/child-views/permissions-display/permissions-display.component.ts
+++ projects/capacitor-calendar/src/app/child-views/permissions-display/permissions-display.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { BaseIonContentComponent } from '../../shared-components/base-ion-content/base-ion-content.component';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-permissions-display',
   templateUrl: './permissions-display.component.html',
   styleUrls: ['./permissions-display.component.scss'],
   standalone: true,
-  imports: [BaseIonContentComponent],
+  imports: [IonContent],
 })
 export class PermissionsDisplayComponent {}
